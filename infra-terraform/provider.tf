d2a4e0241d61d5fb029903cc6ca302f56ac5fa03
Dhruv Dakoria <dhruvdako98@gmail.com>
2021-08-31 02:27:43 -0400

    commenting out required providers

--- infra-terraform/provider.tf
+++ infra-terraform/provider.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.23"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "3.28"
+#     }
+#   }
+# }
 
 # Configure the AWS Provider
 provider "aws" {
