cbc3693149a70f885bcf06712c9e81b1b3de94fa
Dhruv Dakoria <dhruvdako98@gmail.com>
2021-08-31 02:24:45 -0400

    Delete backend.tf

--- infra-terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {  
-    backend "s3" {
-        bucket         = "dhruv-terratest-sf"
-        key            = "terraform-cm-test-devops.tfstate"  
-        region         = "us-east-1"
-        dynamodb_table = "dhruv-terratest-sf"
-    }
-}
